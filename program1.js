f1f0b56a09851585fea4c90aaf0771ad68571be9
Harshit Mishra <94820767+harshitmishra02@users.noreply.github.com>
2024-01-05 13:08:06 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,8 +1,22 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
 var isValid = function(s) {
+     
+   const openSymbols = [];
+    
+    for (let i = 0; i < inputString.length; i++) {
+        if (inputString[i] === '(' || inputString[i] === '{' || inputString[i] === '[') {
+            openSymbols.push(inputString[i]);
+        } else if (inputString[i] === ')' && openSymbols.length !== 0 && openSymbols[openSymbols.length - 1] === '(') {
+            openSymbols.pop();
+        } else if (inputString[i] === '}' && openSymbols.length !== 0 && openSymbols[openSymbols.length - 1] === '{') {
+            openSymbols.pop();
+        } else if (inputString[i] === ']' && openSymbols.length !== 0 && openSymbols[openSymbols.length - 1] === '[') {
+            openSymbols.pop();
+        } else {
+            return false;
+        }
+    }
+    
+    return openSymbols.length === 0;
     
 };
 
